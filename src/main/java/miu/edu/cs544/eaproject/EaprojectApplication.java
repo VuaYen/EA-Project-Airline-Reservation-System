7820de48ff6a1fa73c8562012e88cdcfd6c7dcb2
The Yen <yen637@gmail.com>
2020-06-12 15:17:02 -0500

    initial project

---
+++ src/main/java/miu/edu/cs544/eaproject/EaprojectApplication.java
@@ -0,0 +1,13 @@
+package miu.edu.cs544.eaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EaprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/miu/edu/cs544/eaproject/EaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package miu.edu.cs544.eaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
