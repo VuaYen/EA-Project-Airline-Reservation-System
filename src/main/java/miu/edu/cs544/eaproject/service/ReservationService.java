2f06b77d7d446b9fc82bef6c5f8c738b7d48777d
cuongnguyen <chicuongnguyenit@gmail.com>
2020-06-16 15:55:22 -0500

    implement agent create and confirm reservation

--- src/main/java/miu/edu/cs544/eaproject/service/ReservationService.java
+++ src/main/java/miu/edu/cs544/eaproject/service/ReservationService.java
@@ -2,14 +2,16 @@ package miu.edu.cs544.eaproject.service;
 
 import miu.edu.cs544.eaproject.domain.Reservation;
 import miu.edu.cs544.eaproject.domain.Ticket;
+import miu.edu.cs544.eaproject.service.request.AgentReservationCreateRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 public interface ReservationService {
     public List<Reservation> viewReservations();
-    public Reservation createReservation(Reservation reservation);
+    public Reservation createReservation(Integer flightId, Integer passenger_ID, Integer created_by);
     public List<Reservation> createListReservation(List<Integer> flightIds, Integer passenger_ID, Integer created_by);
+    public List<Reservation> createListReservationByAgent(List<AgentReservationCreateRequest> agentReservationRequests, Integer created_by);
     public Reservation getReservationByCode(String code);
     public List<Ticket> confirmReservation(List<String> flightCodes, Integer passenger_ID);
     public List<Ticket> confirmReservation(List<String> flightCodes);
