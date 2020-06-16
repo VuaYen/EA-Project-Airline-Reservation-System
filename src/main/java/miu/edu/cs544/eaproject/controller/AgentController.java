a7b532aab0e908d6ba93c0f10ef9a3500d36c7d5
The Yen <yen637@gmail.com>
2020-06-16 12:24:39 -0500

    View listAirLineOutAirport LAS

--- src/main/java/miu/edu/cs544/eaproject/controller/AgentController.java
+++ src/main/java/miu/edu/cs544/eaproject/controller/AgentController.java
@@ -4,8 +4,10 @@ package miu.edu.cs544.eaproject.controller;
 import miu.edu.cs544.eaproject.domain.*;
 import miu.edu.cs544.eaproject.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -64,6 +66,12 @@ public class AgentController {
     public List<Ticket> getAllTickets()
     {
         return ticketService.viewTickets();
+    }
+
+    @GetMapping("/airlines/{code}")
+    public List<Airline> getAllAirlinesFlightOutAirport(@PathVariable(name = "code") String code) throws Exception
+    {
+        return flightService.viewAllFlightsOutAirport(code);
     }
 
 }
--- src/main/java/miu/edu/cs544/eaproject/service/FlightServiceImp.java
+++ src/main/java/miu/edu/cs544/eaproject/service/FlightServiceImp.java
@@ -1,6 +1,10 @@
 package miu.edu.cs544.eaproject.service;
 
+import miu.edu.cs544.eaproject.domain.Airline;
+import miu.edu.cs544.eaproject.domain.Airport;
 import miu.edu.cs544.eaproject.domain.Flight;
+import miu.edu.cs544.eaproject.repository.AirlineRepository;
+import miu.edu.cs544.eaproject.repository.AirportRepository;
 import miu.edu.cs544.eaproject.repository.FlightRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,9 +19,20 @@ import java.util.stream.StreamSupport;
 public class FlightServiceImp implements FlightService{
     @Autowired
     private FlightRepository flightRepository;
+    @Autowired
+    private AirlineRepository airlineRepository;
+    @Autowired
+    private AirportRepository airportRepository;
+
     @Override
     public List<Flight> viewAllFlights() {
         return toList(flightRepository.findAll());
+    }
+
+    @Override
+    public List<Airline> viewAllFlightsOutAirport(String airportcode) {
+        Airport airport= airportRepository.findByCode(airportcode);
+        return toList(flightRepository.findAllAirLineByDepartureAirport(airport));
     }
 
     public static <T> List<T> toList(final Iterable<T> iterable) {
