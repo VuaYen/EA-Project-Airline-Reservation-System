6e1e543b956c2048efedeaa9c62c979594971a9a
The Yen <yen637@gmail.com>
2020-06-18 05:35:18 -0500

    update secure tradition

--- traditionalsystem/src/main/java/miu/edu/cs544/eaproject/controller/AdminController.java
+++ traditionalsystem/src/main/java/miu/edu/cs544/eaproject/controller/AdminController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,7 +26,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping("admin")
-//@Secured({ "ROLE_ADMIN" })
+@Secured({ "ROLE_ADMIN" })
 public class AdminController {
 
     @Autowired
